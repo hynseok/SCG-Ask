75a0343c3328e198874d08413ac69162b43942e2
hynseok <wefwef12e@naver.com>
2024-11-04 02:53:54 +0900

    backend: fix bind addr

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,7 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", config.application_port);
+    let address = format!("0.0.0.0:{}", config.application_port);
 
     let listener = TcpListener::bind(address)?;
     run(listener)?.await
