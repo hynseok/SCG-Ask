f417725b5c9486e204836db44f0ad5bc12941303
hynseok <wefwef12e@naver.com>
2024-11-04 10:59:10 +0900

    backend: add logger middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,11 @@
 use backend::{config::get_configuration, startup::run};
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    
     let config = get_configuration().expect("Failed to read configuration.");
     let address = format!("0.0.0.0:{}", config.application_port);
 
